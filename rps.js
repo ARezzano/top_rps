75c62ffbceb922858f85bb643eafa8b790644844
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2023-06-22 11:23:36 -0300

    Add game function that plays 5 rounds and logs match results

--- rps.js
+++ rps.js
@@ -8,18 +8,22 @@ function playRound(playerInput, computerInput){
     computerInput = getComputerChoice();
     let playerInputLowerCase = playerInput.toLowerCase();
     let roundResult = 0;
+    let roundRes = 0; //0 lose, 1 win, 2 tie
 
     switch (playerInputLowerCase){
         case 'rock':
         case 'rocks':
             if(computerInput == "rock"){
                 roundResult = console.log("It's a tie! Rock ties with rock.");
+                roundRes = 2;
             }
             else if(computerInput == "paper"){
                 roundResult = console.log("You lose! Paper beats rock.");
+                roundRes = 0;
             }
             else{
                 roundResult = console.log("You win! Rock beats scissors.");
+                roundRes = 1;
             }
             
             break;
@@ -28,12 +32,15 @@ function playRound(playerInput, computerInput){
         case 'papers':
             if(computerInput == "rock"){
                 roundResult = console.log("You win! Paper beats rock.");
+                roundRes = 1;
             }
             else if(computerInput == "paper"){
                 roundResult = console.log("It's a tie! Paper ties with paper.");
+                roundRes = 2;
             }
             else{
                 roundResult = console.log("You lose! Scissors beats paper.");
+                roundRes = 0;
             }
             
             break;
@@ -42,12 +49,15 @@ function playRound(playerInput, computerInput){
         case 'scissor':
             if(computerInput == "rock"){
                 roundResult = console.log("You lose! Rock beats scissors.");
+                roundRes = 0;
             }
             else if(computerInput == "paper"){
                 roundResult = console.log("You win! Scissors beats paper.");
+                roundRes = 1;
             }
             else{
                 roundResult = console.log("It's a tie! Scissors ties with scissors.");
+                roundRes = 2;
             }
                     
             break;
@@ -57,6 +67,35 @@ function playRound(playerInput, computerInput){
 
     }
 
-    return roundResult;
-    
+    return roundRes;
+
+}
+
+function game(){
+    let playerCounter = 0;
+    let computerCounter = 0;
+
+    for(i = 0; i<5 ; i++){
+
+        playerPrompt = prompt("Round " + (i + 1) + "/5: " + "Choose rock, paper or scissors.");
+        playRound(playerPrompt);
+
+        if(playRound.roundRes == 1){
+            playerCounter++;
+        }
+        
+        if(playRound.roundRes == 0){
+            computerCounter++;
+        }
+    }
+
+    if(playerCounter > computerCounter){
+        return console.log("Game over. You won!");
+    }
+    else if(playerCounter < computerCounter){
+        return console.log("Game over. You lost...");
+    }
+    else{
+        return console.log("Game over. It's a tie!");
+    }
 }
